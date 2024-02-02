7af938c1c81580de83ee64dfa59bfef55db2e9c0
Rubens Rodrigues <paulo.rolim110@gmail.com>
2024-02-02 11:29:36 -0300

    Starting to implement Board and Piece

--- chess-sustem/src/boardgame/Piece.java
+++ chess-sustem/src/boardgame/Piece.java
@@ -1,2 +1,24 @@
-package boardgame;public class Piece {
+package boardgame;
+
+public abstract class Piece {
+    protected Position position;
+    private Board board;
+    public Piece() {
+
+    }
+    public Piece(Board board) {
+        position = null;
+        this.board = board;
+    }
+    protected Board getBoard() {
+        return board;
+    }
+
+    public abstract boolean possibleMoves();
+    public boolean possibleMove(Position position) {
+        return true;
+    }
+    public boolean isThereAnyPossibleMove() {
+        return true;
+    }
 }
